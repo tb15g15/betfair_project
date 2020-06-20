edeb7efa43e241223734894f7963acf47cff201f
tb15g15 <41759285+tb15g15@users.noreply.github.com>
2020-06-20 15:54:25 +0100

    hedging analysis

--- notebooks/historic_data_strategies/strategies/utils.py
+++ notebooks/historic_data_strategies/strategies/utils.py
@@ -1,8 +1,35 @@
 def lay_hedge_threshold(bp, c):
-	return round((bp - 1) * (1 - c) + 1, 2)
+	return (bp - 1) * (1 - c) ** 2 + 1
 
 def back_hedge_threshold(lp, c):
 	return (c ** 2 - 2 * c + lp) / (1 - c) ** 2
 
 def lay_hedge_stake(bp, bs, lp, c):
-    return round((((bp - 1) * bs * (1 - c)) + bs) / (lp - c), 2)
+    return (((bp - 1) * bs * (1 - c)) + bs) / (lp)
+
+def win_payout(row):
+    "DF transformation"
+    if row['stake_2'] == 0:
+        val = - row['stake_1']
+        
+    else:
+        back_payout = (row['price_1'] * row['stake_1']) - row['stake_1']
+        lay_payout = - ((row['price_2'] -1) * row['stake_2'])
+        
+        val = (back_payout +lay_payout) * 0.95
+        
+    return round(val, 2)
+
+def lose_payout(row):
+    "DF transformation"
+    if row['stake_2'] == 0:
+        val = - row['stake_1']
+        
+    else:
+        back_payout = - row['stake_1']
+        lay_payout = row['stake_2'] 
+        
+        val = (back_payout + lay_payout) * 0.95
+        
+    return round(val, 2)
+    
